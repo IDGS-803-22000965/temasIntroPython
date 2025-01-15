7688e53d90d11071ebde692ef02f3581be20ac6a
IDGS-803-22000965 <79228@alumnos.utleon.edu.mx>
2025-01-14 19:15:17 -0600

    Mi primer commit

---
+++ tema2_entradaDatos.py
@@ -0,0 +1,6 @@
+
+print("Dame dos numeros")
+num1=input("numero1: ")
+num2=input("numero2: ")
+res=int(num1)+int(num2)
+print("La suma de {} + {} = {}".format(num1,num2,res))
---
+++ tema4_while.py
@@ -0,0 +1,7 @@
+x=0
+print("Dame un numero para multiplicar")
+num1=input("multiplica el : ")
+while x<=10: 
+    res=int(x)* int(num1)
+    print("La multiplicacion de {} * {} = {}".format(num1,x,res))
+    x=x+1
